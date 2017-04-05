287cc39a3996e249b4ea6f53778c4b10ef97d503
William Kolosque <williammaich@hotmail.com>
2017-04-04 21:37:49 -0300

    adicionei um for

--- src/exemplogit/ExemploGit.java
+++ src/exemplogit/ExemploGit.java
@@ -16,6 +16,10 @@ public class ExemploGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("oi");
+        }
     }
     
 }
